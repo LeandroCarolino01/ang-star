6b097f9e2e73ed1fd2a9f32fe2a96f7e23af2302
leandro <carolino.leandro@gmail.com>
2020-10-07 17:31:07 +0200

    subscription added

--- src/app/star-wars.service.ts
+++ src/app/star-wars.service.ts
@@ -1,5 +1,8 @@
 import { LogService } from './log.service';
 import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs';
+
+
 
 @Injectable()
 export class StarWarsService {
@@ -7,10 +10,13 @@ export class StarWarsService {
     { name: 'Luke Skywalker', side: '' },
     { name: 'Darth Vader', side: '' }
   ];
+
   private logService: LogService;
+  charactersChanged = new Subject<void>();
   constructor(logService: LogService){
     this.logService = logService;
   }
+
   getCharacters(chosenList) {
     if (chosenList=== 'all'){
       return this.characters.slice();
@@ -27,6 +33,7 @@ export class StarWarsService {
       return char.name === charInfo.name;
     })
     this.characters[pos].side = charInfo.side;
+    this.charactersChanged.next();
     this.logService.writeLog('Changed side of ' + charInfo.name + ', new side: ' + charInfo.side);
   }
 
